982b0736bd2555e48cd8a689956fc2c870d4da03
jan_s <jnsbstn391@gmail.com>
2018-03-04 19:59:21 +0700

    fix tgl format

--- routes/tblJurnal.js
+++ routes/tblJurnal.js
@@ -6,7 +6,7 @@ var data = {};
 function convertDate(inputFormat) {
   function pad(s) { return (s < 10) ? '0' + s : s; }
   var d = new Date(inputFormat);
-  return [d.getFullYear(), pad(d.getMonth()+1), pad(d.getDate())].join('-');
+  return [d.getFullYear(), pad(d.getMonth()+1), pad(d.getDate() + 1)].join('-');
 }
 
 var connection = mysql.createConnection({
