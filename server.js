d7528c03f6e6e61e07e9e1509d391bcfba65597e
jan_s <jnsbstn391@gmail.com>
2018-03-04 15:54:01 +0700

    testing

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(function (req,res,next) {
 require("./routes/tblUser")(app);
 require("./routes/tblAkun")(app);
 require("./routes/tblJurnal")(app);
+require("./routes/test")(app);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3100;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
